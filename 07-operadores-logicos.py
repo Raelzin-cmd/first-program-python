cd370cdf87e6a05a5d84465181aaf942f751234c
Israel Almeida <almeidev.rael@gmail.com>
2025-11-18 16:25:05 -0300

    Adicionado operadores logicos

---
+++ 07-operadores-logicos.py
@@ -0,0 +1,25 @@
+idade = 18
+altura = 1.58
+peso = 80
+
+# para participar de uma maratona, a pessoa precisa ter 
+# mais de 17 anos, mais de 1.60 de altura e peso maior que 50
+
+print(idade > 17 and altura > 1.60 and peso > 50)
+
+# para participar de uma maratona, a pessoa precisa ter 
+# mais de 17 anos ou peso maior que 50
+
+print(idade > 17 or peso > 50)
+
+
+
+
+print(True and True) #Verdadeiro
+print(False and True) #Falso
+print(True and False) #Falso
+
+print(True or True) #Verdadeiro
+print(False or True) #Verdadeiro 
+print(True and False) #Verdadeiro
+print(False and False) #False
