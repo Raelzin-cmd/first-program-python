953b7caf57f8d228b28bfeaf019ecb2149949385
Israel Almeida <almeidev.rael@gmail.com>
2025-11-18 15:55:10 -0300

    Adicionado aula sobre operações matematicas

---
+++ 04-operacoes-matematicas.py
@@ -0,0 +1,22 @@
+n1 = 20.0
+n2 = 10
+
+soma = n1 + n2
+print('Soma: ', soma) #printando o resultado
+
+subtracao = n1 - n2
+print('Subtração: ', subtracao)
+
+divisao = n1 / n2
+print('Divisão: ', divisao)
+
+multiplicacao = n1 * n2
+print('Multiplicação: ', multiplicacao)
+
+restoDivisao = n1 % n2
+print('Resto: ', restoDivisao)
+
+par = restoDivisao % 2 #Se o resto da divisao for par irá retornar (0), caso contrário (1)
+print(par)
+
+print(n1**3) #numero 1 elevado ao cubo 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+n1 = 20.0
+n2 = 10
+
+soma = n1 + n2
+print('Soma: ', soma) #printando o resultado
+
+subtracao = n1 - n2
+print('Subtração: ', subtracao)
+
+divisao = n1 / n2
+print('Divisão: ', divisao)
+
+multiplicacao = n1 * n2
+print('Multiplicação: ', multiplicacao)
+
+restoDivisao = n1 % n2
+print('Resto: ', restoDivisao)
