8deebe34ea46d17daf740f9d139a9f33ed4a93c3
Israel Almeida <almeidev.rael@gmail.com>
2025-11-19 16:35:17 -0300

    Adicionado exemplo de dicionario

---
+++ 12-dicionario.py
@@ -0,0 +1,6 @@
+user = {'name': 'Israel', 'email': 'rael@gmail.com', 6: 'abc'}
+
+print(user) #retorna todo o dicionario
+print(user['name']) #retorna o valor atribuido especificamente
+print(user[6]) #retorna o valor atribuido especificamente
+print(user['email']) #retorna o valor atribuido especificamente
