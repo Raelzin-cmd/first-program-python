86e488bb4641b8a93e5d5c1ba8500397c9383abd
Israel Almeida <almeidev.rael@gmail.com>
2025-11-19 16:21:44 -0300

    Adicionado exemplo de tuplas

--- 09-listas.py
+++ 09-listas.py
@@ -3,4 +3,4 @@ nomes = ['Rael', 'Almeida']
 booleanos = [True, False]
 lista = [[1, 3, 5], [2, 4, 6]]
 
-print(lista)
+print(lista) #altere a variavel para printar o exemplo
---
+++ 11-tuplas.py
@@ -0,0 +1,4 @@
+algumaCoisa = True, 'Rael', 123, [], 123
+
+print(algumaCoisa[1]) #pegando dados específicos
+print(algumaCoisa.count(123)) #retorna quantas correspondencias foram encontradas
