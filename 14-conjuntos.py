be2df090f71c9b54bd221257bd76aa8bde6f21d8
Israel Almeida <almeidev.rael@gmail.com>
2025-11-19 17:37:19 -0300

    Exemplos de Conjuntos

---
+++ 14-conjuntos.py
@@ -0,0 +1,7 @@
+frutas = {'maçã', 'uva', 'manga', 'abacaxi', 'uva', 'manga', 'manga'}
+numeros = {1, 13, 42, 3, 1, 10, 1}
+
+print(frutas)
+print(numeros)
+
+#retorna sem obedecer a ordem dos dados e ignora os valores repetidos
