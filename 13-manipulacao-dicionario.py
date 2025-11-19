ae33c319aec59313d75df7b002fe6dc3d3148e75
Israel Almeida <almeidev.rael@gmail.com>
2025-11-19 17:31:45 -0300

    Adicionado exemplos de manipulação de dicionario

---
+++ 13-manipulacao-dicionario.py
@@ -0,0 +1,9 @@
+user = {'name': 'Israel', 'email': 'rael@gmail.com', 6: 'abc'}
+
+print(user) #retorna todo o dicionario
+print(list(user)) #retorna apenas as chaves 
+print(user.get('name')) #retorna o valor especifico
+# user.clear() #limpa o dicionario
+print(user.items()) #retorna a lista de chaves e valores
+print(user.keys()) #retorna apenas a lista com as chaves
+print(user.values()) #retorna apenas os valores das chaves
