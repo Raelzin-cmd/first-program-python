7b8ad52a5c97f1fe92cd65fd1a9e9174da2a49fd
Israel Almeida <almeidev.rael@gmail.com>
2025-11-19 16:12:51 -0300

    adicionado exemplos de manipulação de conjuntos

---
+++ 10-manipulacao-conjuntos.py
@@ -0,0 +1,11 @@
+frutas = {'maçã', 'uva', 'manga', 'abacaxi', 'uva', 'manga', 'manga'}
+numeros = {1, 13, 42, 3, 1, 10, 1}
+
+print(type(frutas))
+print(frutas)
+numeros.add(1)
+print(numeros)
+frutas.remove('manga')
+print(frutas)
+frutas.update('abacaxi')
+print(frutas)
